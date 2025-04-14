42548b526810726140db72f37222f62c19b6d058
Amit_KR_Singh <amitkumarsingh21feb2005@gmail.com>
2025-04-14 22:07:47 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (63.34%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 0; // Pointer to track the position to overwrite
+        
+        for (int num : nums) {
+            // If we are at the first 2 elements or the current number is different from nums[j-2]
+            if (j < 2 || num > nums[j - 2]) {
+                nums[j++] = num;
+            }
+        }
+        
+        return j; // New length of modified array
+    }
+}
