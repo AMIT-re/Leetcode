25ff388a81374029cbe0ab23065244509ae90a4d
Amit_KR_Singh <amitkumarsingh21feb2005@gmail.com>
2025-05-21 09:38:28 +0530

    Time: 5 ms (56.51%) | Memory: 43.1 MB (83.49%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        int c[]=new int[256];
+        for(int i=0;i<s.length();i++)
+        {
+            c[s.charAt(i)]++;
+            c[t.charAt(i)]--;
+        }
+        for(int i=0;i<c.length;i++)
+        {
+            if(c[i]!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
