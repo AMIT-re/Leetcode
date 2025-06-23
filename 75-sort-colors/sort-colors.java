fa17a6487fd190f387ed54e2226665fc81eb873c
Amit_KR_Singh <amitkumarsingh21feb2005@gmail.com>
2025-06-23 17:15:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (49.74%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,60 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) {
-        sort(nums, 0, nums.length - 1);
-
-        // Optional: print array
-        System.out.print("[");
-        for (int i = 0; i < nums.length; i++) {
-            if (i != nums.length - 1) {
-                System.out.print(nums[i] + ",");
-            } else {
-                System.out.print(nums[i]);
-            }
-        }
-        System.out.println("]");
-    }
-
-    // Merge Sort
-    public static void sort(int[] arr, int start, int end) {
-        if (start < end) {
-            int middle = (start + end) / 2;
-            sort(arr, start, middle);
-            sort(arr, middle + 1, end);
-            merge(arr, start, middle, end);
-        }
-    }
-
-    public static void merge(int[] arr, int start, int mid, int end) {
-        int n1 = mid - start + 1;
-        int n2 = end - mid;
-
-        int[] left = new int[n1];
-        int[] right = new int[n2];
-
-        for (int i = 0; i < n1; i++) {
-            left[i] = arr[start + i];
-        }
-
-        for (int j = 0; j < n2; j++) {
-            right[j] = arr[mid + 1 + j];
-        }
-
-        int i = 0, j = 0, k = start;
-
-        while (i < n1 && j < n2) {
-            if (left[i] <= right[j]) {
-                arr[k++] = left[i++];
-            } else {
-                arr[k++] = right[j++];
-            }
-        }
-
-        while (i < n1) {
-            arr[k++] = left[i++];
-        }
-
-        while (j < n2) {
-            arr[k++] = right[j++];
-        }
+        Arrays.sort(nums);
+        
     }
 }
