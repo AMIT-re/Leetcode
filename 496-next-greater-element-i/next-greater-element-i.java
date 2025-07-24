b9e136ac59025dd38c3779b4319b5e7198845cc0
Amit_KR_Singh <amitkumarsingh21feb2005@gmail.com>
2025-07-24 18:13:45 +0530

    Time: 5 ms (17.01%) | Memory: 43.2 MB (86.70%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,28 +1,52 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        //int[] ans=new int[nums1.length];
-        Deque<Integer> st=new ArrayDeque<>();
-        HashMap<Integer,Integer> mp=new HashMap<>();
-        for(int i=nums2.length-1;i>=0;i--){
-             while(!st.isEmpty() && nums2[i]>st.peek()){
-                st.pop();
-             }
-            if(st.size()<0){
-                mp.put(nums2[i],-1);
-
+        int res[]=new int[nums2.length];
+        Stack<Integer> str=new Stack<>();
+        for(int i=nums2.length-1;i>=0;i--)
+        {
+            if(str.isEmpty())
+            {
+                str.push(nums2[i]);
+                res[i]=-1;
+            }             
+            else if(nums2[i]<str.peek())
+            {   
+                res[i]=str.peek();
+                str.push(nums2[i]);
+                
             }
-            else if(!st.isEmpty() && nums2[i]<st.peek()){
-                mp.put(nums2[i],st.peek());
-
+            else
+            {
+                while(!str.isEmpty() && nums2[i]>=str.peek())
+                {
+                    str.pop();
+                }
+                if(str.isEmpty())
+                {
+                    res[i]=-1;
+                }
+                else{
+                res[i]=str.peek();
+                }
+                str.push(nums2[i]);
             }
-            else{
-                mp.put(nums2[i],-1);
-            }
-            st.push(nums2[i]);
         }
-        for(int i=0;i<nums1.length;i++){
-            nums1[i]=mp.get(nums1[i]);
+
+        for(int i=0;i<nums1.length;i++)
+        {
+            int x=nums1[i];
+            for(int j=0;j<nums2.length;j++)
+            {
+                if(nums2[j]==x)
+                {
+                    nums1[i]=res[j];
+                    break;
+                }
+            }            
         }
+
         return nums1;
+        
+        
     }
 }
