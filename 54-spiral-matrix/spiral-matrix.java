3ac2b74df8f2e506d8707d7499b29dc842023856
Amit_KR_Singh <amitkumarsingh21feb2005@gmail.com>
2025-08-30 10:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (42.99%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,38 +1,42 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new ArrayList<>();
-        int top = 0, bottom = matrix.length - 1;
-        int left = 0, right = matrix[0].length - 1;
-
-        while (top <= bottom && left <= right) {
-            // Top row
-            for (int i = left; i <= right; i++) {
-                res.add(matrix[top][i]);
+        List<Integer> ll=new ArrayList<>();
+        int top=0;
+        int bottom=matrix.length-1;
+        int left=0;
+        int right=matrix[0].length-1;
+        while(top<=bottom && left<=right)
+        {
+            for(int i=left;i<=right;i++)
+            {
+                ll.add(matrix[top][i]);
             }
             top++;
-
-            // Right column
-            for (int i = top; i <= bottom; i++) {
-                res.add(matrix[i][right]);
+            for(int i=top;i<=bottom;i++)
+            {
+                ll.add(matrix[i][right]);
             }
             right--;
-
-            if (top <= bottom) {
-                // Bottom row
-                for (int i = right; i >= left; i--) {
-                    res.add(matrix[bottom][i]);
-                }
-                bottom--;
+            if(top<=bottom){
+            for(int i=right;i>=left;i--)
+            {
+                ll.add(matrix[bottom][i]);
             }
-
-            if (left <= right) {
-                // Left column
-                for (int i = bottom; i >= top; i--) {
-                    res.add(matrix[i][left]);
+            bottom--;
+            }
+            if(left<=right)
+            {
+                for(int i=bottom;i>=top;i--)
+                {
+                    ll.add(matrix[i][left]);
                 }
                 left++;
             }
+            
         }
-        return res;
+
+        return ll;
+
+        
     }
 }
